4704ea1b71939a28d75e7ed74a57371668d131af
Thomas Weng <thomas.weng11@gmail.com>
2017-06-03 23:23:18 -0700

    Replaced initial page with registration page Moved old components to separate folder

--- src/main/webapp/front-end/src/index.jsx
+++ src/main/webapp/front-end/src/index.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import {setState, loadTriggerElementSubTypes, loadTriggerElementSubTypesSuccess, loadTriggerElementSubTypesError, cleanState} from './action_creators';
-import App from './components/App';
-import {List, Map} from 'immutable';
 import promise from 'redux-promise';
-import {AppGridContainer} from './components/AppGrid';
-//import {MapWrapperContainer} from './components/MapWrapper'
+import App from './components/App';
+import Register from './components/Register';
 
 const createStoreWithMiddleware = applyMiddleware(
   promise
 )(createStore);
 const store = createStoreWithMiddleware(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 
 store.dispatch(cleanState());
 
@@ -24,11 +21,8 @@ store.dispatch(loadTriggerElementSubTypes()).then((response) => {
            });
 
 const routes = <Route component={App}>
-  <Route path="/" component={AppGridContainer} />
-  <Route path="/adventures/:adventureId/edit" component={AppGridContainer} />
+  <Route path="/" component={Register} />
 </Route>;
-
-
 
 ReactDOM.render(
   <Provider store={store}>
