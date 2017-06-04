81139892adc282cee0c0a4ddc8df4832828ce404
Thomas Weng <thomas.weng11@gmail.com>
2017-06-04 00:48:26 -0700

    Moved AppGrid layout into App class Create distinct route for /register

--- src/main/webapp/front-end/src/components/App.jsx
+++ src/main/webapp/front-end/src/components/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import NavBar from './NavBar';
 
-export default React.createClass({
-  render: function() {
-    return React.cloneElement(this.props.children);
+export default class App extends React.Component {
+  render() {
+    return <Grid>
+        <Row>
+            <Col>
+                <NavBar />
+            </Col>
+        </Row>
+        <Row className="show-grid">
+            <Col>
+              { React.cloneElement(this.props.children) }
+            </Col>
+        </Row>
+    </Grid>
   }
-});
+}
