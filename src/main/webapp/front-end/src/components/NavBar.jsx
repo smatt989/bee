7784f21031ea9b30af6a0419ca79b39bd4bfbbdf
Thomas Weng <thomas.weng11@gmail.com>
2017-06-06 22:36:00 -0700

    Clean up navbar

--- src/main/webapp/front-end/src/components/NavBar.jsx
+++ src/main/webapp/front-end/src/components/NavBar.jsx
@@ -12,19 +12,12 @@ export default class NavBar extends React.Component {
             </Navbar.Header>
             <Navbar.Collapse>
               <Nav>
-                <NavItem eventKey={1} href="#">Link</NavItem>
-                <NavItem eventKey={2} href="#">Link</NavItem>
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                  <MenuItem eventKey={3.1}>Action</MenuItem>
-                  <MenuItem eventKey={3.2}>Another action</MenuItem>
-                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </NavDropdown>
+                <NavItem eventKey={1} href="#">Tasks</NavItem>
+                <NavItem eventKey={2} href="#">Images</NavItem>
               </Nav>
               <Nav pullRight>
-                <NavItem eventKey={1} href="#">Link Right</NavItem>
-                <NavItem eventKey={2} href="#">Link Right</NavItem>
+                <NavItem eventKey={1} href="#">Log In</NavItem>
+                <NavItem eventKey={2} href="#/register">Sign Up</NavItem>
               </Nav>
             </Navbar.Collapse>
           </Navbar>;
