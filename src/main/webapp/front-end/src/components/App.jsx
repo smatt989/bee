3bda99276287c22d7fc69302a6ba3d2d311bfee8
Thomas Weng <thomas.weng11@gmail.com>
2017-06-06 23:14:27 -0700

    Upgraded to react-router v4 Added react-router bootstrap for links Removed underscore

--- src/main/webapp/front-end/src/components/App.jsx
+++ src/main/webapp/front-end/src/components/App.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
 import NavBar from './NavBar';
+import Home from './Home';
+import Register from './Register';
+import Err from './Error';
 
 export default class App extends React.Component {
   render() {
-    return <Grid>
+    return <Router>
+      <Grid>
         <Row>
-            <Col>
-                <NavBar />
-            </Col>
+          <Col>
+            <NavBar />
+          </Col>
         </Row>
         <Row className="show-grid">
-            <Col>
-              { React.cloneElement(this.props.children) }
-            </Col>
+          <Col>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/register" component={Register}/>
+              <Route path="/" component={Err}/>
+            </Switch>
+          </Col>
         </Row>
-    </Grid>
+      </Grid>
+    </Router>
   }
 }
--- src/main/webapp/front-end/src/components/NavBar.jsx
+++ src/main/webapp/front-end/src/components/NavBar.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import {Navbar, NavItem, NavDropdown, MenuItem, Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import {Navbar, NavItem, Nav} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 export default class NavBar extends React.Component {
   render() {
     return <Navbar inverse collapseOnSelect>
             <Navbar.Header>
               <Navbar.Brand>
-                <a href="#">Bee</a>
+                <Link to="/">Bee</Link>
               </Navbar.Brand>
               <Navbar.Toggle />
             </Navbar.Header>
             <Navbar.Collapse>
               <Nav>
-                <NavItem eventKey={1} href="#">Tasks</NavItem>
-                <NavItem eventKey={2} href="#">Images</NavItem>
+                <LinkContainer to="#"><NavItem eventKey={1}>Tasks</NavItem></LinkContainer>
+                <LinkContainer to="#"><NavItem eventKey={2}>Images</NavItem></LinkContainer>
               </Nav>
               <Nav pullRight>
-                <NavItem eventKey={1} href="#">Log In</NavItem>
-                <NavItem eventKey={2} href="#/register">Sign Up</NavItem>
+                <LinkContainer to="#"><NavItem eventKey={1}>Log In</NavItem></LinkContainer>
+                <LinkContainer to="/register"><NavItem eventKey={2}>Sign Up</NavItem></LinkContainer>
               </Nav>
             </Navbar.Collapse>
           </Navbar>;
