950c8b37c927d307b53777e658c12deb22d54af0
Thomas Weng <thomas.weng11@gmail.com>
2017-06-06 23:19:53 -0700

    Remove unnecessary packages

--- src/main/webapp/front-end/src/components/NavBar.jsx
+++ src/main/webapp/front-end/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Navbar, NavItem, Nav} from 'react-bootstrap';
-import {LinkContainer} from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class NavBar extends React.Component {
   render() {
