52faf974475f8e69a0b9002ed6d42a79de6cc797
Thomas Weng <thomas.weng11@gmail.com>
2017-06-06 23:14:27 -0700

    Upgraded to react-router v4 Added react-router bootstrap for links Removed underscore

---
+++ src/main/webapp/front-end/src/components/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Err extends React.Component {
+  render() {
+    return <h1>Error</h1>;
+  }
+}
--- src/main/webapp/front-end/src/index.jsx
+++ src/main/webapp/front-end/src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import {SET_USER} from './actions';
 import promise from 'redux-promise';
 import App from './components/App';
-import Home from './components/Home';
-import Register from './components/Register';
 
 const initialState = {
   userId: null
@@ -22,14 +19,9 @@ const store = createStoreWithMiddleware(reducer, initialState, window.__REDUX_DE
 // Set fake user id to test redux
 store.dispatch({ type: SET_USER, userId: 1 });
 
-const routes = <Route component={App}>
-  <Route path="/" component={Home} />
-  <Route path="/register" component={Register} />
-</Route>;
-
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>{routes}</Router>
+    <App />
   </Provider>,
   document.getElementById('app')
 );
