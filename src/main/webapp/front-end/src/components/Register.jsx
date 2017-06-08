5c0e9590be8ad94fd3c599ba2b8bce4f58c6a860
Thomas Weng <thomas.weng11@gmail.com>
2017-06-07 23:21:05 -0700

    Add Login forms

--- src/main/webapp/front-end/src/components/Register.jsx
+++ src/main/webapp/front-end/src/components/Register.jsx
@@ -10,14 +10,17 @@ import PasswordFormGroup from './account_forms/PasswordFormGroup.jsx'
 
 export default class Register extends React.Component {
   render() {
+    const emailInputProps = { isSignup: true, placeholder: "Enter your email" };
+    const pwInputProps = { isSignup: true, placeholder: "Choose a password" };
+
     // TODO onSubmit validation, prevent submission if error
     return <div>
         <Row>
           <h1>Register</h1>
         </Row>
         <form role="form" action="/auth/signup" method="post">
-          <Row><EmailFormGroup /></Row>
-          <Row><PasswordFormGroup /></Row>
+          <Row><EmailFormGroup emailInputProps={emailInputProps}/></Row>
+          <Row><PasswordFormGroup pwInputProps={pwInputProps}/></Row>
           <Row>
             <Button
               bsStyle="primary"
