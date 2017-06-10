d0a7839ebeeb901d81b67abeafecf43fb66fc6f9
Thomas <thomas@Thomass-MacBook-Pro.local>
2017-06-10 00:08:16 -0700

    Remove unnecessary Row components

--- src/main/webapp/front-end/src/components/Home.jsx
+++ src/main/webapp/front-end/src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, PageHeader } from 'react-bootstrap';
 
 export default class Home extends React.Component {
   render() {
     return <Grid>
-      <h1>Home</h1>
+      <PageHeader>Home</PageHeader>
     </Grid>;
   }
 }
--- src/main/webapp/front-end/src/components/Images.jsx
+++ src/main/webapp/front-end/src/components/Images.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, PageHeader } from 'react-bootstrap';
 
 export default class Images extends React.Component {
   render() {
     return <Grid>
-      <h1>Images</h1>
+      <PageHeader>Images</PageHeader>
     </Grid>;
   }
 }
