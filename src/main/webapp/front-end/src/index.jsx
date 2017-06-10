bf5977a366bc36b49de0ed51662a36349d27f952
Thomas <thomas@Thomass-MacBook-Pro.local>
2017-06-09 22:56:25 -0700

    Remove stub SET_USER action

--- src/main/webapp/front-end/src/index.jsx
+++ src/main/webapp/front-end/src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
-import {SET_USER} from './actions';
 import promise from 'redux-promise';
 import App from './components/App.jsx';
 
@@ -15,9 +14,6 @@ const createStoreWithMiddleware = applyMiddleware(
   promise
 )(createStore);
 const store = createStoreWithMiddleware(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-// Set fake user id to test redux
-store.dispatch({ type: SET_USER, userId: 1 });
 
 ReactDOM.render(
   <Provider store={store}>
