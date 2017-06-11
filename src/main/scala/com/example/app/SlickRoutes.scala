6e39997115a8cd7bb176061d9774a1beb3d60668
Matthew Slotkin <matt@Matthews-MacBook-Pro.local>
2017-06-11 17:41:58 -0400

    should be good for all headers now

--- src/main/scala/com/example/app/SlickRoutes.scala
+++ src/main/scala/com/example/app/SlickRoutes.scala
@@ -1,5 +1,6 @@
 package com.example.app
 
+import com.example.app.models.{Image, ImageSource}
 import org.scalatra.{CorsSupport, FutureSupport, ScalatraBase}
 //import slick.driver.H2Driver.api._
 import slick.driver.PostgresDriver.api._
@@ -18,7 +19,8 @@ trait SlickRoutes extends ScalatraBase with FutureSupport with JacksonJsonSuppor
   }
 
   before() {
-    response.setHeader("Access-Control-Expose-Headers", SessionTokenStrategy.HeaderKey)
+    val imageSourceTypeFieldHeaders = ImageSource.imageSourceTypes.flatMap(_.fields)
+    response.setHeader("Access-Control-Expose-Headers", (Seq(SessionTokenStrategy.HeaderKey, Image.configsHeader) ++ imageSourceTypeFieldHeaders).mkString(", "))
   }
 
 }
