95defa86fe4f81dcfcb329b214db90d472db5350
Thomas <thomas@Thomass-MacBook-Pro.local>
2017-06-11 01:55:09 -0700

    dispatch login action on login form submit

--- src/main/webapp/front-end/src/components/Login.jsx
+++ src/main/webapp/front-end/src/components/Login.jsx
@@ -6,7 +6,7 @@ import {
   Button  
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
-import { loginEmailChanged, loginPasswordChanged } from '../actions.js'
+import { loginEmailChanged, loginPasswordChanged, login } from '../actions.js'
 import EmailFormGroup from './account_forms/EmailFormGroup.jsx'
 import PasswordFormGroup from './account_forms/PasswordFormGroup.jsx'
 
@@ -49,9 +49,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         // TODO onSubmit validation, prevent submission if error
-        onSubmit: (email, password) => {
-          console.log(email, password);
-        }
+        onSubmit: (email, password) => dispatch(login(email, password))
     }
 }
 
