9532cd0f5a7249b42802799f40e355323fb03de0
Thomas <thomas@Thomass-MacBook-Pro.local>
2017-06-11 20:23:30 -0700

    Add redirect on sign in

--- src/main/webapp/front-end/src/components/Login.jsx
+++ src/main/webapp/front-end/src/components/Login.jsx
@@ -5,7 +5,7 @@ import {
   PageHeader,
   Button  
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { loginEmailChanged, loginPasswordChanged, login, loginSuccess, loginError } from '../actions.js'
 import EmailFormGroup from './account_forms/EmailFormGroup.jsx'
 import PasswordFormGroup from './account_forms/PasswordFormGroup.jsx'
@@ -13,15 +13,25 @@ import PasswordFormGroup from './account_forms/PasswordFormGroup.jsx'
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirectToReferrer: false
+    }
+
     this.onSubmit = (e) => {
       e.preventDefault();
-      this.props.onSubmit(this.props.email, this.props.password);
+      this.props.onSubmit(this.props.email, this.props.password)
+        .then(response => this.setState({ redirectToReferrer: response }))
     }
   }
 
   render() {
     const emailInputProps = { value: this.props.email, validation: null, placeholder: "Enter your email", action: (email) => loginEmailChanged(email) };
     const pwInputProps = { value: this.props.password, validation: null, placeholder: "Enter your password", action: (password) => loginPasswordChanged(password) };
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    
+    if (this.state.redirectToReferrer) {
+      return <Redirect to={from} />
+    }
 
     return <Grid>
         <PageHeader>Log in</PageHeader>
@@ -40,23 +50,29 @@ class Login extends React.Component {
 }
 
 const mapStateToProps = state => {
-    return {
-        email: state.get('loginEmail').get('email'),
-        password: state.get('loginPassword').get('password')
-    }
+  return {
+      email: state.get('loginEmail').get('email'),
+      password: state.get('loginPassword').get('password')
+  }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        // TODO onSubmit validation, prevent submission if error
-        onSubmit: (email, password) => {
-          dispatch(login(email, password))
-          .then(response => {
-            const session = !response.error ? response.payload.headers["bee-session-key"] : null;
-            !response.error ? dispatch(loginSuccess(session)) : dispatch(loginError(response.payload.error));
-          })
-        }
+  return {
+    // TODO onSubmit validation, prevent submission if error
+    onSubmit: (email, password) => {
+      return dispatch(login(email, password))
+        .then(response => {
+          if (response.error) {
+            dispatch(loginError(response.error));
+            return false;
+          }
+
+          const session = response.payload.headers["bee-session-key"];
+          dispatch(loginSuccess(session));
+          return true;
+        })
     }
+  }
 }
 
 const LoginContainer = connect(
