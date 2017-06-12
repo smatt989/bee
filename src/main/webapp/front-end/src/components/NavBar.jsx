72ccd03a13ba4311c4eaa86133e468ca1623f3f5
Thomas <thomas@Thomass-MacBook-Pro.local>
2017-06-11 21:50:02 -0700

    Handle logout button press

--- src/main/webapp/front-end/src/components/NavBar.jsx
+++ src/main/webapp/front-end/src/components/NavBar.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout, logoutError, logoutSuccess } from '../actions.js';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
+const NavRight = (props) => {
+  console.log(props);
+  return props.session != null ? 
+    <Nav pullRight>
+      <NavItem eventKey={2} onClick={() => props.logout(props.session)}>Log Out</NavItem>
+    </Nav> : 
+    <Nav pullRight>
+      <LinkContainer to="/login"><NavItem eventKey={1}>Log In</NavItem></LinkContainer>
+      <LinkContainer to="/register"><NavItem eventKey={2}>Sign Up</NavItem></LinkContainer>
+    </Nav>
+}
+
 class NavBar extends React.Component {
   render() {
-    const NavRight = () => {
-      return this.props.loggedIn ? 
-        <Nav pullRight>
-          <LinkContainer to="/logout"><NavItem eventKey={2}>Log Out</NavItem></LinkContainer>
-        </Nav> : 
-        <Nav pullRight>
-          <LinkContainer to="/login"><NavItem eventKey={1}>Log In</NavItem></LinkContainer>
-          <LinkContainer to="/register"><NavItem eventKey={2}>Sign Up</NavItem></LinkContainer>
-        </Nav>
-    }
-
     return <Navbar inverse collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
@@ -29,7 +31,7 @@ class NavBar extends React.Component {
           <LinkContainer to="/tasks"><NavItem eventKey={1}>Tasks</NavItem></LinkContainer>
           <LinkContainer to="/images"><NavItem eventKey={2}>Images</NavItem></LinkContainer>
         </Nav>
-        <NavRight />
+        <NavRight session={this.props.session} logout={this.props.logout}/>
       </Navbar.Collapse>
     </Navbar>;
   }
@@ -37,12 +39,30 @@ class NavBar extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        loggedIn: state.get('login').get('session') != null
+        session: state.get('login').get('session')
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        logout: (session) => {
+          return dispatch(logout(session))
+            .then(response => {
+              if (response.error) {
+                dispatch(logoutError(response.error));
+                return false;
+              }
+
+              dispatch(logoutSuccess(response.payload));
+              return true;
+            })
+        }
     }
 }
 
 const NavBarContainer = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(NavBar)
 
 export default NavBarContainer;
