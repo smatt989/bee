13fef81d496e06cebe346504d9d0e7d7e93ea594
Thomas Weng <thomas.weng11@gmail.com>
2017-06-16 01:13:34 -0700

    Added link to detailed task view from task table Added route for task view Added stub for detailed Task view

---
+++ src/main/webapp/front-end/src/components/tasks/ViewTask.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { 
+  PageHeader,
+} from 'react-bootstrap';
+
+const ViewTask = (props) => {
+  return <PageHeader>View Task</PageHeader>;
+}
+
+export default ViewTask;
