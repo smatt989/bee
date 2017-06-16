9f4add9a7d2e773038b185698eb6cc284082c242
Thomas Weng <thomas.weng11@gmail.com>
2017-06-16 01:13:34 -0700

    Added link to detailed task view from task table Added route for task view Added stub for detailed Task view

--- src/main/webapp/front-end/src/components/tasks/TasksTableItem.jsx
+++ src/main/webapp/front-end/src/components/tasks/TasksTableItem.jsx
@@ -4,20 +4,25 @@ import {
   Button,
   ButtonGroup
 } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-const TasksTableItem = (props) => {
-  const { data } = props;
-  return <tr>
-    <td>{data.id}</td>
-    <td>{data.name}</td>
-    <td>
-      <ButtonGroup>
-        <Button>Left</Button>
-        <Button>Middle</Button>
-        <Button>Right</Button>
-      </ButtonGroup>
-    </td>
-  </tr>
+class TasksTableItem extends React.Component {
+  render() {
+    const { data } = this.props;
+    return <tr>
+      <td>{data.id}</td>
+      <td>{data.name}</td>
+      <td>
+        <ButtonGroup>
+          <Button>Left</Button>
+          <Button>Middle</Button>
+          <LinkContainer to={'/tasks/' + data.id + '/view'}>
+            <Button>View</Button>
+          </LinkContainer>
+        </ButtonGroup>
+      </td>
+    </tr>
+  }
 }
 
 const mapStateToProps = state => {
