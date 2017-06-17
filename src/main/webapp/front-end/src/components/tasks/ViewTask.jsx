e391d86bf3ffb524118ca6a5caa8e7601c0e61e2
Thomas Weng <thomas.weng11@gmail.com>
2017-06-16 20:07:58 -0700

    Connect View Task view with API action Fix routing issue in TasksTableItem

--- src/main/webapp/front-end/src/components/tasks/ViewTask.jsx
+++ src/main/webapp/front-end/src/components/tasks/ViewTask.jsx
@@ -4,22 +4,59 @@ import {
   Grid,
   PageHeader
 } from 'react-bootstrap';
+import { viewTask, viewTaskSuccess, viewTaskError } from '../../actions.js';
 
-const ViewTask = (props) => {
-  return <Grid>
-    <PageHeader>
-      Manage Task
-    </PageHeader>
-  </Grid>;
+class ViewTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      isError: false
+    };
+
+    this.props.getTask(this.props.match.params.id)
+      .then(isSuccess => this.setState({ isLoading: false, isError: isSuccess }));
+  }
+
+  render() {
+    const { task } = this.props;
+    const body = this.state.isLoading ? 
+      <div>Loading</div> : 
+      <div>
+        <div>Id: { task.id }</div>
+        <div>Name: { task.name }</div>
+      </div>
+
+    return <Grid>
+      <PageHeader>
+        View Task
+      </PageHeader>
+      { body } 
+    </Grid>;
+  }
 }
 
 const mapStateToProps = state => {
+  const currentTask = state.get('currentTask').toJS().task;
   return {
+    task: currentTask ? currentTask.data : null
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
+    getTask: (id) => {
+      return dispatch(viewTask(id))
+        .then(response => {
+          if (response.error) {
+            dispatch(viewTaskError(response.error));
+            return false;
+          }
+
+          dispatch(viewTaskSuccess(response.payload));
+          return true;
+        });
+    }
   }
 }
 
