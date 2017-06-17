0db608b44165b7e570485eb0f5d1f621e7fc0ca6
Thomas Weng <thomas.weng11@gmail.com>
2017-06-16 20:40:53 -0700

    Simplified TasksTableItem to functional component Added error and loading states for TasksTable

--- src/main/webapp/front-end/src/components/tasks/TasksTableItem.jsx
+++ src/main/webapp/front-end/src/components/tasks/TasksTableItem.jsx
@@ -6,38 +6,20 @@ import {
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-class TasksTableItem extends React.Component {
-  render() {
-    const { data, match } = this.props;
-    return <tr>
-      <td>{data.id}</td>
-      <td>{data.name}</td>
-      <td>
-        <ButtonGroup>
-          <LinkContainer to={`${match.url}/${data.id}/view`}>
-            <Button>Manage</Button>
-          </LinkContainer>
-          <Button>Label</Button>
-          <Button>Leave</Button>
-        </ButtonGroup>
-      </td>
-    </tr>
-  }
+const TasksTableItem = ({ data, match }) => {
+  return <tr>
+    <td>{data.id}</td>
+    <td>{data.name}</td>
+    <td>
+      <ButtonGroup>
+        <LinkContainer to={`${match.url}/${data.id}/view`}>
+          <Button>Manage</Button>
+        </LinkContainer>
+        <Button>Label</Button>
+        <Button>Leave</Button>
+      </ButtonGroup>
+    </td>
+  </tr>
 }
 
-const mapStateToProps = state => {
-  return {
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-  }
-}
-
-const TasksTableItemContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TasksTableItem)
-
-export default TasksTableItemContainer;
+export default TasksTableItem;
