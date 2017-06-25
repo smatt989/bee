83f8bc783240ceb8da6a9649fa62c5fc8baf6eb8
matt <matthew.slotkin@gmail.com>
2017-06-25 01:26:11 -0700

    added new label form.  no validation.  not properly hooked up to workflow

--- src/main/webapp/front-end/src/store.js
+++ src/main/webapp/front-end/src/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-import { cleanState } from './actions';
+import { cleanState, ontologyTypes, ontologyTypesSuccess, ontologyTypesError } from './actions';
 import reducer from './reducer';
 import promise from 'redux-promise';
 
@@ -9,8 +9,22 @@ function initStore() {
   )(createStore);
   const store = createStoreWithMiddleware(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   store.dispatch(cleanState());
+  fetchReferenceData(store);
   return store;
 }
 
 const store = initStore();
 export default store;
+
+function fetchReferenceData(store) {
+    store.dispatch(ontologyTypes())
+                .then(response => {
+                    if(response.error) {
+                        store.dispatch(ontologyTypesError(response.error));
+                        return false;
+                    }
+
+                    store.dispatch(ontologyTypesSuccess(response.payload.data));
+                    return true;
+                });
+}
