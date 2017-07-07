cd341326c82259ebdf1121222daf994678972749
Matthew Slotkin <matt@Matthews-MacBook-Pro.local>
2017-07-07 01:48:16 -0700

    added simple button to image sources to get back to task.  will probably want to add more than just a button (like name of task...)

--- src/main/webapp/front-end/src/components/image_sources/ImageSources.jsx
+++ src/main/webapp/front-end/src/components/image_sources/ImageSources.jsx
@@ -8,6 +8,14 @@ export default class ImageSources extends React.Component {
   render() {
     const taskId = this.props.match.params.id
     return <Grid>
+        <LinkContainer to={'/tasks/'+taskId+'/view'}>
+          <Button
+            className="new-tbl-item-btn"
+            bsStyle="primary"
+            type="button">
+            Back to Task
+          </Button>
+        </LinkContainer>
       <PageHeader>
         Image Sources
         <LinkContainer className="pull-right" to={'/tasks/'+taskId+'/image-sources/new'}>
