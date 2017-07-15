61c0580bb422cac81e1f6296fcf0768c3cc32753
Amadeus Junqueira <amadeus.junqueira@MBP13060.lan>
2017-07-15 17:49:45 -0400

    Fix build issues!

--- src/main/webapp/front-end/webpack.config.js
+++ src/main/webapp/front-end/webpack.config.js
@@ -1,9 +1,18 @@
 var path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const mkdirp = require('mkdirp');
+const copydir = require('copy-dir');
 
 const extractLess = new ExtractTextPlugin({
   filename: '[name].css'
 });
+// fix TW Bootstrap Issue
+mkdirp.sync('./node_modules/bootstrap/less/fonts/', () => console.log(arguments[0]))
+copydir.sync(
+  './node_modules/transferwise-iconfont/fonts/',
+  './node_modules/bootstrap/less/fonts/'
+);
+
 module.exports = {
   devtool: 'eval',
   entry: [
